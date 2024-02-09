5623b03ced77b76492c86f7a927e42647a25c6be
ericx1107 <xushiyu1107@gmail.com>
2024-02-09 13:24:10 +1100

    Add robot test script

--- test_robot.py
+++ test_robot.py
@@ -13,11 +13,12 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.transforms as transforms
 import machinevisiontoolbox as mvtb
+import matplotlib.pyplot as plt
 script_path = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.abspath(os.path.join(script_path, "../PenguinPi-robot/software/python/client/")))
 from pibot_client import PiBot
 
-from model import AlexNet
+from model import *
 
 CUDA_AVALIABLE = torch.cuda.is_available()
 
@@ -38,7 +39,7 @@ bot.setVelocity(0, 0)
 #LOAD NETWORK WEIGHTS HERE
 base_path = "/Users/eric/Documents/phd/RVSS_Need4Speed/"
 model_path = base_path + "self-driving-robot/models/" + args.model
-nn_model = torch.load(model_path)
+nn_model = torch.load(model_path, map_location="cpu")
 
 
 if CUDA_AVALIABLE:
@@ -58,47 +59,77 @@ time.sleep(1)
 print("GO!")
 
 stop_blocker = 0
+stopped_last = False
+
+idx = 0
+
+angles_list = [0.0]
 
 try:
     angle = 0
     while True:
+        start = time.time()
+        
         # get an image from the the robot
         img_array = bot.getImage() # numpy ndarray
+
+        # crop image
+        img_array = img_array[np.ceil(img_array.shape[0]/2).astype(int):, :]
 
         cv2.imshow('image', img_array)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             bot.setVelocity(0, 0)
             break
 
-        # convert to opencv
+        
+
+        # convert to rgb
+        img_array = cv2.cvtColor(img_array, cv2.COLOR_BGR2RGB)
+
         
 
         #TO DO: apply any necessary image transforms
         
-        # crop image
-        # img = img[np.ceil(img.shape[0]/2).astype(int):, :]
 
         #TO DO: pass image through network get a prediction
+        # print(img_array.shape)
+        # plt.imshow(img_array)
+        # plt.show()
         img = torch.tensor(img_array).float().unsqueeze(0).permute(0, 3, 1, 2)
+        
+        
+        # img = img - img.mean(axis=(1, 2, 3), keepdims=True)
+        
         
         if CUDA_AVALIABLE:
             img.cuda()
         
-        angle = nn_model(img)
+        # angles = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.2, 0.2, 0.2, 0.2, 0.2, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+        # angle = angles[idx]
+        # idx += 1
+        # start = time.time()
+        
+        idx += 1
+        angle = nn_model(img)# torch.tensor(angles_list[idx-1]))
+        angles_list.append(angle)
+        # print(f"inf time: {time.time() - start}")
+
         print(f"angle: {angle.item()}")
         
         
         #TO DO: convert prediction into a meaningful steering angle
-        Kd = 10 #base wheel speeds, increase to go faster, decrease to go slower
-        Ka = 20 #how fast to turn when given an angle
+        Kd = 20 #base wheel speeds, increase to go faster, decrease to go slower
+        # if abs(angle) < 0.1:
+            # Kd = 40
+        Ka = 40 #how fast to turn when given an angle
         left  = int(Kd + Ka*angle)
         right = int(Kd - Ka*angle)
 
-        #TO DO: check for stop signs?
-        img_array = cv2.cvtColor(img_array, cv2.COLOR_BGR2HSV)
+        # #TO DO: check for stop signs?
+        img_array = cv2.cvtColor(img_array, cv2.COLOR_RGB2HSV)
         
         img_array = cv2.blur(img_array, (5, 5))
-        
+                
         red = img_array * np.expand_dims(img_array[:, :, 0] >= 170, 2)
         red = red * np.expand_dims(180 >= img_array[:, :, 0], 2)
         red = red * np.expand_dims(img_array[:, :, 1] >= 130, 2)
@@ -108,14 +139,20 @@ try:
         stop_blocker -= 1
         
         # pixels 500
-        if pixels >= 1500 and stop_blocker <= 0:
-            # stop for some time
-            print("\n\n\n================ STOPPED ===============\n\n\n")
-            stop_iters = 30
-            bot.setVelocity(0, 0)
-            time.sleep(5)
+        if pixels >= 800: #stop_blocker <= 0:
+            if not stopped_last:
+                # stop for some time
+                print("\n\n\n================ STOPPED ===============\n\n\n")
+                stopped_last = True
+                bot.setVelocity(0, 0)
+                time.sleep(5)
+        else:
+            stopped_last = False
+            stop_blocker = 40
             
         bot.setVelocity(left, right)
+        print(time.time() - start)
+        # time.sleep(0.08)
             
         
 except KeyboardInterrupt:    
